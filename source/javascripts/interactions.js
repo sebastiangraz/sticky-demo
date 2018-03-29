6b2744fe60ab1abff7f01661bbeec0e53f82e074
Sebastian Graz <sebastian.graz@spindriftgroup.com>
2018-03-29 14:43:23 +0100

    added sticky mobile nav

--- source/javascripts/interactions.js
+++ source/javascripts/interactions.js
@@ -1,12 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-
   $('[data-role*="TOGGLE"]').on('click', function () {
     $(this)
       .find('[data-fa-i2svg]')
       .toggleClass('fa-angle-up')
       .toggleClass('fa-angle-down');
     $(this).parent().next().toggleClass('hidden')
+  });
+
+  $('[data-role*="OPENFILTER"]').on('click', function(){
+    $('body').addClass('overflow-hidden');
+    $('[data-role*="OVERLAY"]').addClass('open').removeClass('closed')
+    $('[data-role*="MOBILESIDEBAR"]').addClass('open').removeClass('closed')
+  })
+
+  $('[data-role*="OVERLAY"]').on('click', function(){
+    $('body').removeClass('overflow-hidden');
+    $('[data-role*="MOBILESIDEBAR"]').addClass('closed').removeClass('open')
+    $(this).addClass('closed').removeClass('open')
+  })
+
+  $('[data-role*="COUNTER"]').hide();
+
+  $('[data-role*="CHECKBOX"]').each(function( ) {
+    $(this).on('click', function(){
+      var count = document.querySelectorAll('[data-role*="CHECKBOX"]:checked').length;
+      $('[data-role*="COUNTER"]').show();
+      $('[data-role*="COUNTER"]').html(count);
+      if (count == 0) {
+        $('[data-role*="COUNTER"]').hide();
+      }
+    })
   });
 
   // document.querySelector('.toggle').addEventListener("click", purchase, false)
