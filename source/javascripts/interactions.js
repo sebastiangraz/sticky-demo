93d1eb2275a673f496caf011a0f45a57e4aaeb34
Sebastian Graz <sebastian.graz@spindriftgroup.com>
2018-04-04 14:19:38 +0100

    sidebar state checkpoint

--- source/javascripts/interactions.js
+++ source/javascripts/interactions.js
@@ -1,12 +1,51 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  $('[data-role*="TOGGLE"]').on('click', function () {
-    $(this)
-      .find('[data-fa-i2svg]')
-      .toggleClass('fa-angle-up')
-      .toggleClass('fa-angle-down');
-    $(this).parent().next().toggleClass('hidden')
-  });
+  let toggleText = function(elem, t1, t2){
+    if (elem.innerHTML === t1) {
+      elem.innerHTML = t2;
+    } else {
+      elem.innerHTML = t1
+    }
+  };
+
+  let toggleState = function (elem, one, two) {
+    elem.setAttribute('data-state', elem.getAttribute('data-state') === one ? two : one);
+  };
+
+  // document.querySelector('[data-role="HIDEFILTERS"]').onclick = function () {
+  //   toggleText(this.querySelector('span'), 'Hide all filters', 'Show all filters');
+  //
+  //   let test = document.querySelectorAll('[data-role="TOGGLELIST"]');
+  //   for (var e of test) {
+  //     toggleState(e, 'closed', 'open');
+  //   }
+  // };
+
+  let sidebarArray = document.querySelectorAll('[data-role="TOGGLE"]');
+
+  Array.from(sidebarArray).forEach(a =>
+    a.onclick = function () {
+      console.log();
+      toggleState(this.parentElement.nextElementSibling, 'closed', 'open');
+    }
+    // console.log(a.parentElement)
+  )
+
+  // $('[data-role*="HIDEFILTERS"]').on('click', function(){
+  //   let test = document.querySelectorAll('[data-role="TOGGLELIST"]');
+  //   for (var e of test) {
+  //     console.log(e);
+  //     toggleState(e, 'closed', 'open');
+  //   }
+  // });
+
+  // $('[data-role*="TOGGLE"]').on('click', function(){
+  //
+  // });
+
+  // $('[data-role*="TOGGLE"]').on('click', function () {
+  //   $(this).parent().next().toggleClass('hidden')
+  // });
 
   $('[data-role*="OPENFILTER"]').on('click', function(){
     $('body').addClass('overflow-hidden');
@@ -20,7 +59,9 @@ document.addEventListener('DOMContentLoaded', function () {
     $(this).addClass('closed').removeClass('open')
   })
 
+
   $('[data-role*="COUNTER"]').hide();
+
 
   $('[data-role*="CHECKBOX"]').each(function( ) {
     $(this).on('click', function(){
